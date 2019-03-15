2b0ecc7a1ae0cade79f06a4d055efaf370cfc1b7
karan kumar <karan@antrepriz.com>
2019-03-15 14:15:11 +0530

    link remove

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import First from './Container/First';
-import Second from './Container/Second';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <Route exact path = "/" component = {First } />
-          <Route path = "/airtel" component = {Second} />
       </div>
     );
   }
--- src/Container/First/index.js
+++ src/Container/First/index.js
@@ -3,8 +3,6 @@ import './index.css';
 import Header from '../Header';
 import team from './team.jpg';
 import Points from '../Points';
-import airtel from './airtel.jpg';
-import {Link } from 'react-router-dom';
 
 class First extends React.Component {
 
@@ -17,9 +15,6 @@ class First extends React.Component {
                 <Header />
                 <ImageFull />
                 <Points />
-                <Link to = '/airtel'>
-                     <img style = {{padding:'10px 60px'}} src = {airtel} alt = "airtel logo" width = "150px" height = "100px" />
-                </Link>
             </div>
         )
     }
